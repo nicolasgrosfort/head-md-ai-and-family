33f27a0e14e98145f00862786eab8ba27b772ded
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-03-11 14:22:36 +0100

    feat: setup fal api

--- server/index.js
+++ server/index.js
@@ -1,19 +1,41 @@
-const express = require("express");
-const cors = require("cors");
-const os = require("os");
+import cors from "cors";
+import express from "express";
+import os from "os";
 
 const hostname = os.hostname();
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
-app.use(express.json());
+//   await fal.config({
+//     apiKey:
+//       "ed3171f3-6338-4853-a827-49b60822a802:cbba78d2e33b5670fb722a76f55a5ce2",
+//   });
 
-app.get("/test", (req, res) => {
-  res.json({
-    status: "ok",
-    time: new Date(),
-  });
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/test", async (req, res) => {
+  try {
+    const { audio } = req.body;
+
+    console.log("audio received length:", audio.length);
+
+    // const result = await fal.subscribe("fal-ai/speech-to-text", {
+    //   input: {
+    //     audio_url: audio,
+    //   },
+    //   logs: true,
+    // });
+
+    res.json({
+      status: "ok",
+      length: audio.length,
+      //   text: result.data.output.
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "transcription failed" });
+  }
 });
 
 app.listen(PORT, "0.0.0.0", () => {
---
+++ server/readme.md
@@ -0,0 +1,8 @@
+# Server
+
+## Setup FAL AI Api Key
+
+```bash
+export FAL_KEY="my-api-key"
+```
+
