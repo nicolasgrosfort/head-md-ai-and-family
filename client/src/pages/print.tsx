da29450bd279b21cd85fb111f1152c8fb301305a
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-03-12 17:06:21 +0100

    feat: print story and title

--- client/src/pages/print.tsx
+++ client/src/pages/print.tsx
@@ -1,16 +1,29 @@
+import { useAtomValue } from "jotai";
+import { storyAtom, storyTitleAtom } from "../store/atoms";
 import { printTsukumogami } from "../utils/endpoints";
 
 export const Print = () => {
+  const storyTitle = useAtomValue(storyTitleAtom);
+  const story = useAtomValue(storyAtom);
+
+  const readyToPrint = storyTitle && story;
+
   return (
     <main className="h-dvh w-dvw flex items-center justify-center">
-      <button
-        className="text-3xl font-mono font-bold cursor-pointer"
-        onClick={async () => {
-          printTsukumogami(["Line 1", "Line 2", "Line 3"]);
-        }}
-      >
-        PRINT
-      </button>
+      {readyToPrint ? (
+        <button
+          className="text-3xl font-mono font-bold cursor-pointer"
+          onClick={async () => {
+            printTsukumogami([storyTitle, story]);
+          }}
+        >
+          PRINT
+        </button>
+      ) : (
+        <p className="text-2xl font-mono italic text-gray-500">
+          Share a memory to enable printing
+        </p>
+      )}
     </main>
   );
 };
