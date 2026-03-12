49a4419131de34efb9e28b30d83993240953bb82
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-03-12 17:42:05 +0100

    feat: normalize text printer

--- server/libs/printer.js
+++ server/libs/printer.js
@@ -24,6 +24,14 @@ serialPort.on("open", () => {
 
 serialPort.on("error", (err) => console.error("Erreur port série:", err));
 
+function normalizeText(text) {
+  return text
+    .replace(/[\u2018\u2019\u201A\u201B\u2032\u2035]/g, "'") // curly apostrophes
+    .replace(/[\u201C\u201D\u201E\u201F\u2033\u2036]/g, '"') // curly quotes
+    .replace(/[\u2013\u2014]/g, "-") // em/en dash
+    .replace(/\u2026/g, "..."); // ellipsis
+}
+
 async function resizeForPrinter(imagePath) {
   const tmpPath = join(tmpdir(), `printer-${Date.now()}.png`);
 
@@ -73,7 +81,7 @@ export async function print(items = []) {
       switch (item.type) {
         case "text":
           if (item.bold) job = job.bold(true);
-          job = job.printLine(item.content ?? "");
+          job = job.printLine(normalizeText(item.content ?? ""));
           if (item.bold) job = job.bold(false);
           break;
         case "newline":
